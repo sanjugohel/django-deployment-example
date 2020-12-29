2868719c359c2703024acb3a4e7be4d5cad23873
sanjugohel <71930648+sanjugohel@users.noreply.github.com>
2020-12-29 15:01:18 +0530

    Update settings.py

--- learn_templates/settings.py
+++ learn_templates/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')yp3g&pfl)def^*h*2=l6-cf@ckxm1ehzla2_^7t#oxx60ip^^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sanjugohel.pythonanywhere.com']
 
 
 # Application definition
